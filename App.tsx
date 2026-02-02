d16d84c1251d080d2c172268579431858f8058ba
Douglas Duarte <douglasduarte.eng@gmail.com>
2026-02-02 07:54:55 -0300

    chore: initial project setup with React Native and TailwindCSS

---
+++ App.tsx
@@ -0,0 +1,9 @@
+
+import "./src/styles/global.css";
+import { Sign } from '@/screens/sign/sign';
+
+export default function App() {
+  return (
+    <Sign />
+  );
+}
---
+++ src/screens/sign/sign.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export function Sign() {
+
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Sign Screen</Text>
+    </View>
+  )
+}
