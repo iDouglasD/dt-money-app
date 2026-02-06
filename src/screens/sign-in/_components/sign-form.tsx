53ce32a0f203d6ab0f5d4aae887ab276d614183d
Douglas Duarte <douglasduarte.eng@gmail.com>
2026-02-06 14:11:32 -0300

    feat(sign-form): add sign-in and registration buttons to the form

--- src/screens/sign-in/_components/sign-form.tsx
+++ src/screens/sign-in/_components/sign-form.tsx
@@ -1,6 +1,8 @@
 import { useFormContext } from "react-hook-form"
 import { SignInSchema } from "../_validations/sign-in-schema"
 import { Input } from "@/components/input"
+import { Button } from "@/components/button"
+import { Text, View } from "react-native"
 
 export function SignForm() {
   const { control } = useFormContext<SignInSchema>()
@@ -17,11 +19,24 @@ export function SignForm() {
       <Input
         control={control}
         name="password"
-        label="SENHA"
-        placeholder="Sua senha"
+        label="PASSWORD"
+        placeholder="Your password"
         leftIconName="lock-outline"
         secureTextEntry
       />
+      <View className="flex-1 justify-between mt-8 mb-6 min-h-[250px]">
+        <Button iconName="arrow-forward">
+          Sign In
+        </Button>
+        <View>
+          <Text className="mb-6 text-gray-300 text-base">
+            Don't have an account?
+          </Text>
+          <Button iconName="arrow-forward" mode="outline">
+            Register
+          </Button>
+        </View>
+      </View>
     </>
   )
 }
