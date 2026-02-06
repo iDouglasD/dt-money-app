0c33b3e005c9d34c031a785adf3c86ac11b10c9e
Douglas Duarte <douglasduarte.eng@gmail.com>
2026-02-06 14:58:49 -0300

    feat(sign-in): enhance SignForm with validation and submission handling

--- src/screens/sign-in/_components/sign-form.tsx
+++ src/screens/sign-in/_components/sign-form.tsx
@@ -1,11 +1,25 @@
-import { useFormContext } from "react-hook-form"
-import { SignInSchema } from "../_validations/sign-in-schema"
+import { useForm } from "react-hook-form"
+import { signInSchema, SignInSchema } from "../_validations/sign-in-schema"
 import { Input } from "@/components/input"
-import { Button } from "@/components/button"
-import { Text, View } from "react-native"
+import { Text, View } from "react-native";
+import { Button } from "@/components/button";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { PublicStackParamsList } from "@/routes/public-routes/public-routes";
+import { zodResolver } from '@hookform/resolvers/zod'
+
 
 export function SignForm() {
-  const { control } = useFormContext<SignInSchema>()
+  const { control, handleSubmit, formState: { errors, isSubmitting } } = useForm<SignInSchema>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    }
+  })
+
+  const navigation = useNavigation<NavigationProp<PublicStackParamsList>>()
+
+  async function onSubmit({ email, password }: SignInSchema) { }
 
   return (
     <>
@@ -25,14 +39,18 @@ export function SignForm() {
         secureTextEntry
       />
       <View className="flex-1 justify-between mt-8 mb-6 min-h-[250px]">
-        <Button iconName="arrow-forward">
+        <Button iconName="arrow-forward" onPress={handleSubmit(onSubmit)} disabled={isSubmitting}>
           Sign In
         </Button>
         <View>
           <Text className="mb-6 text-gray-300 text-base">
             Don't have an account?
           </Text>
-          <Button iconName="arrow-forward" mode="outline">
+          <Button
+            iconName="arrow-forward"
+            mode="outline"
+            onPress={() => navigation.navigate("Register")}
+          >
             Register
           </Button>
         </View>
--- src/screens/sign-in/_validations/sign-in-schema.ts
+++ src/screens/sign-in/_validations/sign-in-schema.ts
@@ -1,8 +1,8 @@
 import z from "zod"
 
 export const signInSchema = z.object({
-  email: z.email(),
-  password: z.string().nullable(),
+  email: z.email("Invalid email format").min(1, "Email is required"),
+  password: z.string().min(6, "Password must be at least 6 characters").min(1, "Password is required"),
 })
 
 export type SignInSchema = z.infer<typeof signInSchema>
