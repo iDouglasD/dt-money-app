c9cdc5f0aebed895e90dac5b3e84277b36249d74
Douglas Duarte <douglasduarte.eng@gmail.com>
2026-02-11 08:50:35 -0300

    feat: add date-fns for date formatting and update TransactionCard component to display last transaction date

--- src/screens/home/_components/transaction-card.tsx
+++ src/screens/home/_components/transaction-card.tsx
@@ -1,8 +1,11 @@
 import { TransactionTypes } from "@/shared/enums/transaction-types";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Text, View } from "react-native";
-import { ICONS } from "../_helper/card-color";
-import { CARD_DATA } from "../_helper/card-data";
+import { useTransactions } from "@/shared/hooks/use-transactions";
+import { format } from "date-fns";
+import { ca } from "zod/v4/locales";
+import { ICONS } from "../_strategies/card-color";
+import { CARD_DATA } from "../_strategies/card-data";
 
 export type TransactionCardType = TransactionTypes | "total"
 
@@ -12,8 +15,16 @@ interface TransactionCardProps {
 }
 
 export function TransactionCard({ type, amount }: TransactionCardProps) {
+  const { transactionsResult } = useTransactions()
+
   const iconData = ICONS[type]
   const cardData = CARD_DATA[type]
+
+  const transactions = transactionsResult?.data
+  const lastTransaction = transactions?.find(({ type: transactionType }) => transactionType.id === type)
+
+  const hasCreatedAt = !!lastTransaction?.createdAt
+  const isNotTotalCard = type !== "total"
 
   return (
     <View className={`bg-${cardData.bgColor} min-w-[280] rounded-[6] px-8 py-6 justify-between mr-6`}>
@@ -31,6 +42,16 @@ export function TransactionCard({ type, amount }: TransactionCardProps) {
         <Text className="text-2xl text-gray-400 font-bold">
           R$ {amount.toFixed(2).replace(".", ",")}
         </Text>
+        {
+          isNotTotalCard && (
+            <Text className="text-gray-700">
+              {hasCreatedAt && (
+                format(lastTransaction.createdAt, `'Last ${cardData.label.toLocaleLowerCase()} on' MMMM dd`)
+              )}
+              {!hasCreatedAt && "No transactions"}
+            </Text>
+          )
+        }
       </View>
     </View>
   )
