64f6ba445e7f75ef8b6719b5c88e4e0f0b75bcfe
Douglas Duarte <douglasduarte.eng@gmail.com>
2026-02-11 09:16:23 -0300

    feat: refactor ListHeader and TransactionCard components to use TransactionHeaderCard and improve transaction handling

--- src/screens/home/_components/transaction-card.tsx
+++ src/screens/home/_components/transaction-card.tsx
@@ -1,59 +1,65 @@
+import { colors } from "@/shared/colors";
 import { TransactionTypes } from "@/shared/enums/transaction-types";
+import { Transaction } from "@/shared/interfaces/https/transaction-interface";
 import { MaterialIcons } from "@expo/vector-icons";
-import { Text, View } from "react-native";
-import { useTransactions } from "@/shared/hooks/use-transactions";
+import clsx from "clsx";
 import { format } from "date-fns";
-import { ca } from "zod/v4/locales";
-import { ICONS } from "../_strategies/card-color";
-import { CARD_DATA } from "../_strategies/card-data";
-
-export type TransactionCardType = TransactionTypes | "total"
+import { Text, View } from "react-native";
+import Swipeable from "react-native-gesture-handler/ReanimatedSwipeable";
 
 interface TransactionCardProps {
-  type: TransactionCardType
-  amount: number
+  transaction: Transaction
 }
 
-export function TransactionCard({ type, amount }: TransactionCardProps) {
-  const { transactionsResult } = useTransactions()
+export function TransactionCard({ transaction }: TransactionCardProps) {
 
-  const iconData = ICONS[type]
-  const cardData = CARD_DATA[type]
-
-  const transactions = transactionsResult?.data
-  const lastTransaction = transactions?.find(({ type: transactionType }) => transactionType.id === type)
-
-  const hasCreatedAt = !!lastTransaction?.createdAt
-  const isNotTotalCard = type !== "total"
+  const isExpenseType = transaction.type.id === TransactionTypes.EXPENSE
 
   return (
-    <View className={`bg-${cardData.bgColor} min-w-[280] rounded-[6] px-8 py-6 justify-between mr-6`}>
-      <View className="flex-row justify-between items-center mb-1">
+    <Swipeable
+      containerStyle={{
+        alignItems: "center",
+        alignSelf: "center",
+        overflow: "visible",
+        width: "90%",
+        marginBottom: 16
+      }}
+    >
+      <View className="h-[140] bg-background-tertiary rounded-[6] p-6">
         <Text className="text-white text-base">
-          {cardData.label}
+          {transaction.description}
         </Text>
-        <MaterialIcons
-          name={iconData.name}
-          color={iconData.color}
-          size={26}
-        />
-      </View>
-      <View>
-        <Text className="text-2xl text-gray-400 font-bold">
-          R$ {amount.toFixed(2).replace(".", ",")}
+        <Text
+          className={clsx("text-2xl font-bold mt-2",
+            isExpenseType ? "text-accent-red" : "text-accent-brand-light")
+          }
+        >
+          {isExpenseType && "-"}
+          R$ {transaction.value.toFixed(2).replace(".", ",")}
         </Text>
-        {
-          isNotTotalCard && (
-            <Text className="text-gray-700">
-              {hasCreatedAt && (
-                format(lastTransaction.createdAt, `'Last ${cardData.label.toLocaleLowerCase()} on' MMMM dd`)
-              )}
-              {!hasCreatedAt && "No transactions"}
+        <View className="flex-row w-full justify-between items-center">
+          <View className="items-center flex-row mt-3">
+            <MaterialIcons
+              name="label-outline"
+              color={colors.gray[700]}
+              size={23}
+            />
+            <Text className="text-gray-700 text-base ml-2">
+              {transaction.category.name}
             </Text>
-          )
-        }
+          </View>
+          <View className="items-center flex-row mt-3">
+            <MaterialIcons
+              name="calendar-month"
+              color={colors.gray[700]}
+              size={20}
+            />
+            <Text className="text-gray-700 text-base ml-2">
+              {format(transaction.createdAt, "MM/dd/yyyy")}
+            </Text>
+          </View>
+        </View>
       </View>
-    </View>
+    </Swipeable>
   )
 }
-
